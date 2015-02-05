eafc81ab6d1cef2eb2e7119f83064010cc89a060
Ross Girshick <ross.girshick@gmail.com>
2015-02-04 21:36:21 -0800

    init checkin

---
+++ finetuning.py
@@ -0,0 +1,150 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from keyboard import keyboard
+
+SCALES          = (480, 576, 688, 864, 1200)
+BATCH_SIZE      = 128
+FG_FRACTION     = 0.25
+FG_THRESH       = 0.5
+BG_THRESH_HI    = 0.5
+BG_THRESH_LO    = 0.1
+PIXEL_MEANS     = np.array([[[102.9801, 115.9465, 122.7717]]])
+FEAT_STRIDE     = 16
+MAX_SIZE        = 2000
+
+def sample_rois(labels, overlaps, rois, fg_rois_per_image, rois_per_image):
+    """Generate a random sample of ROIs comprising foreground and background
+    examples.
+
+    Args:
+      labels (1d np array): class labels from a window db
+      overlaps (1d np array): class max overlap from a window db
+      rois (2d np array): regions of interest in image coordinates, one per row
+      fg_rois_per_image (int): target number of foreground ROIs
+      rois_per_image (int): target number of ROIs to return in total
+
+    Returns:
+      labels (1d np array)
+      overlaps (1d np array)
+      rois (2d np array)
+    """
+    # Select foreground ROIs
+    fg_inds = np.where(overlaps >= FG_THRESH)[0]
+    fg_rois_per_this_image = np.minimum(fg_rois_per_image, fg_inds.size)
+    fg_inds = np.random.choice(fg_inds, size=fg_rois_per_this_image,
+                               replace=False)
+    # Select background ROIs
+    bg_inds = np.where((overlaps < BG_THRESH_HI) &
+                       (overlaps >= BG_THRESH_LO))[0]
+    bg_rois_per_this_image = rois_per_image - fg_rois_per_this_image
+    bg_rois_per_this_image = np.minimum(bg_rois_per_this_image,
+                                        bg_inds.size)
+    bg_inds = np.random.choice(bg_inds, size=bg_rois_per_this_image,
+                               replace=False)
+    keep_inds = np.append(fg_inds, bg_inds)
+    labels[bg_inds] = 0
+    labels = labels[keep_inds]
+    overlaps = overlaps[keep_inds]
+    rois = rois[keep_inds]
+    return labels, overlaps, rois
+
+def get_image_blob(window_db, scale_inds, do_flip):
+    """Build an input blob from the images in the window db at the specified
+    scales.
+    """
+    num_images = len(window_db)
+    max_shape = (0, 0, 0)
+    processed_ims = []
+    im_scale_factors = []
+    for i in xrange(num_images):
+        im = cv2.imread(window_db[i]['image'])
+        # if do_flip:
+        #     im = im[:, ::-1, :]
+        im = im.astype(np.float32, copy=False)
+        im -= PIXEL_MEANS
+        im_shape = im.shape
+        im_size = np.min(im_shape[0:2])
+        im_size_big = np.max(im_shape[0:2])
+        target_size = SCALES[scale_inds[i]]
+        im_scale = float(target_size) / float(im_size)
+        # Prevent the biggest axis from being more than MAX_SIZE
+        if np.round(im_scale * im_size_big) > MAX_SIZE:
+            im_scale = float(MAX_SIZE) / float(im_size_big)
+        im = cv2.resize(im, None, None, fx=im_scale, fy=im_scale,
+                        interpolation=cv2.INTER_LINEAR)
+        im_scale_factors.append(im_scale)
+        processed_ims.append(im)
+        max_shape = np.maximum(max_shape, im.shape)
+
+    blob = np.zeros((num_images, max_shape[0],
+                     max_shape[1], max_shape[2]))
+    for i in xrange(num_images):
+        im = processed_ims[i]
+        blob[i, 0:im.shape[0], 0:im.shape[1], :] = im
+    # Move channels (axis 3) to axis 1
+    channel_swap = (0, 3, 1, 2)
+    blob = blob.transpose(channel_swap)
+    return blob, im_scale_factors
+
+def map_im_rois_to_feat_rois(im_rois, im_scale_factor):
+    feat_rois = np.round(im_rois * im_scale_factor / FEAT_STRIDE)
+    return feat_rois
+
+def get_minibatch(window_db, random_flip=True):
+    # Decide to flip the entire batch or not
+    # do_flip = False if not random_flip else bool(np.random.randint(0, high=2))
+    num_images = len(window_db)
+    # Sample random scales to use for each image in this batch
+    random_scale_inds = np.random.randint(0, high=len(SCALES), size=num_images)
+    assert(BATCH_SIZE % num_images == 0), 'num_images must divide BATCH_SIZE'
+    rois_per_image = BATCH_SIZE / num_images
+    fg_rois_per_image = np.round(FG_FRACTION * rois_per_image)
+    # Get the input blob, formatted for caffe
+    # Takes care of random scaling and flipping
+    do_flip = False
+    im_blob, im_scale_factors = get_image_blob(window_db,
+                                               random_scale_inds, do_flip)
+    # Now, build the region of interest and label blobs
+    rois_blob = np.zeros((0, 5), dtype=np.float32)
+    labels_blob = np.zeros((0), dtype=np.float32)
+    all_overlaps = []
+    for im_i in xrange(num_images):
+        labels = window_db[im_i]['windows'][:, 0]
+        overlaps = window_db[im_i]['windows'][:, 1]
+        im_rois = window_db[im_i]['windows'][:, 2:]
+        # if do_flip:
+        #     im_rois[:, (0, 2)] = window_db[im_i]['width'] - \
+        #                          im_rois[:, (2, 0)] - 1
+        labels, overlaps, im_rois = sample_rois(labels, overlaps, im_rois,
+                                                fg_rois_per_image,
+                                                rois_per_image)
+        feat_rois = map_im_rois_to_feat_rois(im_rois, im_scale_factors[im_i])
+        rois_blob_this_image = \
+            np.append(im_i * np.ones((feat_rois.shape[0], 1)), feat_rois,
+                      axis=1)
+        rois_blob = np.append(rois_blob, rois_blob_this_image, axis=0)
+        labels_blob = np.append(labels_blob, labels, axis=0)
+        all_overlaps = np.append(all_overlaps, overlaps, axis=0)
+    # vis_minibatch(im_blob, rois_blob, labels_blob, all_overlaps)
+    return im_blob, rois_blob, labels_blob
+
+def vis_minibatch(im_blob, rois_blob, labels_blob, overlaps):
+    num_images = im_blob.shape[0]
+    for i in xrange(rois_blob.shape[0]):
+      rois = rois_blob[i, :]
+      im_ind = rois[0]
+      roi = rois[1:] * FEAT_STRIDE
+      im = im_blob[im_ind, :, :, :].transpose((1, 2, 0)).copy()
+      im += PIXEL_MEANS
+      im = im[:, :, (2, 1, 0)]
+      im = im.astype(np.uint8)
+      cls = labels_blob[i]
+      plt.imshow(im)
+      print 'class: ', cls, ' overlap: ', overlaps[i]
+      plt.gca().add_patch(
+          plt.Rectangle((roi[0], roi[1]), roi[2] - roi[0],
+                        roi[3] - roi[1], fill=False,
+                        edgecolor='r', linewidth=3)
+          )
+      plt.show()
---
+++ pyramid_train.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+
+# Define train prototxt with two inputs:
+#  0: pyramid batch
+#  1: ROI inputs
+# Loop:
+#   get next window_db entry
+#   load image with cv2
+#   use deep pyramid library to construct input image batch (pyramid)
+#   use deep pyramid library to maps image space ROIs into pyramid ROIs
+#   use deep pyramid library to compute label for each ROI
+#   prefill solver's net with two input blobs
+#   execute solver step(1)
+
+from deep_pyramid import DeepPyramid
+import sys
+caffe_path = '../caffe/python'
+sys.path.insert(0, caffe_path)
+import caffe
+import numpy as np
+import cPickle
+import gzip
+import cv2
+import time
+import matplotlib.pyplot as plt
+import finetuning
+from keyboard import keyboard
+
+def print_label_stats(labels):
+    counts = np.bincount(labels.astype(np.int))
+    for i in xrange(len(counts)):
+        if counts[i] > 0:
+            print '{:3d}: {:4d}'.format(i, counts[i])
+
+def load_window_db(path):
+    with gzip.GzipFile(path, 'rb') as f:
+        return cPickle.load(f)
+
+def load_solver_and_window_db(solver_def_path, window_db_path,
+                              pretrained_model=None):
+    solver = caffe.SGDSolver(solver_def_path)
+    if pretrained_model is not None:
+        print 'Loading pretrained model weights from {:s}' \
+            .format(pretrained_model)
+        solver.net.copy_from(pretrained_model)
+    print 'Loading window db from {:s}'.format(window_db_path)
+    window_db = load_window_db(window_db_path)
+    return solver, window_db
+
+def train_model(solver_def_path, window_db_path, pretrained_model=None,
+                GPU_ID=None):
+    solver, window_db = \
+        load_solver_and_window_db(solver_def_path,
+                                  window_db_path,
+                                  pretrained_model=pretrained_model)
+    caffe.set_phase_train()
+    caffe.set_mode_gpu()
+    if GPU_ID is not None:
+        caffe.set_device(GPU_ID)
+
+    max_epochs = 100
+    dp = DeepPyramid(solver.net)
+    for epoch in xrange(max_epochs):
+        # TODO(rbg): shuffle window_db
+        for db_i in xrange(len(window_db)):
+            start_t = time.time()
+
+            # Load image and prepare pyramid input batch
+            im = cv2.imread(window_db[db_i]['image'])
+            im_pyra, pyra_scales = dp.get_image_pyramid(im)
+            im_pyra_batch = dp.image_pyramid_to_batch(im_pyra)
+
+            # Load boxes and convert to pyramid ROIs
+            im_rois = window_db[db_i]['windows'][:, 2:]
+            pyra_levels, pyra_rois = \
+                dp.im_to_feat_pyramid_coords(im_rois, pyra_scales)
+            pyra_levels_and_rois = \
+                np.append(pyra_levels[:, np.newaxis], pyra_rois, axis=1)
+            # TODO(rbg): remove duplicate pyra_rois (due to roi aliasing)
+            # map (class, overlap) to hard label
+            labels = window_db[db_i]['windows'][:, 0]
+            overlaps = window_db[db_i]['windows'][:, 1]
+            # Label all ROIs with < 0.5 overlap as background (label = 0)
+            fg_inds = np.where(overlaps >= 0.5)
+            bg_inds = np.where(overlaps < 0.5)
+            labels[bg_inds] = 0
+
+            # Take a random sample of the examples
+            bg_inds = np.asarray(bg_inds).T
+            fg_inds = np.asarray(fg_inds).T
+            np.random.shuffle(bg_inds)
+            num_to_sample = 1500 - fg_inds.size
+            keep_inds = np.append(fg_inds,
+                                  bg_inds[0:num_to_sample], axis=0).ravel()
+            pyra_levels_and_rois = pyra_levels_and_rois[keep_inds]
+            labels = labels[keep_inds]
+            im_rois = im_rois[keep_inds]
+
+#            for c in xrange(1, 21):
+#                plt.imshow(im)
+#                c_inds = np.where(labels == c)[0]
+#                if c_inds.size == 0:
+#                    continue
+#                print 'class: ', c
+#                for fg_i in c_inds:
+#                    roi = im_rois[fg_i, :]
+#                    plt.gca().add_patch(
+#                        plt.Rectangle((roi[0], roi[1]), roi[2] - roi[0],
+#                                      roi[3] - roi[1], fill=False,
+#                                      edgecolor='r', linewidth=3)
+#                        )
+#                plt.show()
+
+            # Reshape net's input blobs
+            base_shape = im_pyra[0].shape
+            num_rois = pyra_levels_and_rois.shape[0]
+            solver.net.blobs['data'].reshape(dp.num_levels, base_shape[2],
+                                             base_shape[0], base_shape[1])
+            solver.net.blobs['rois'].reshape(num_rois, 5, 1, 1)
+            solver.net.blobs['labels'].reshape(num_rois, 1, 1, 1)
+            # Copy data into net's input blobs
+            solver.net.blobs['data'].data[...] = im_pyra_batch
+            solver.net.blobs['rois'].data[...] = \
+                pyra_levels_and_rois[:, :, np.newaxis, np.newaxis]
+            solver.net.blobs['labels'].data[...] = \
+                labels[:, np.newaxis, np.newaxis, np.newaxis]
+
+            print 'epoch {:d} image {:d}'.format(epoch, db_i)
+            print_label_stats(labels)
+            solver.step(1)
+            # Periodically snapshot and test
+            print 'Elapsed time: {:.4f}'.format(time.time() - start_t)
+
+def train_model_random_scales(solver_def_path, window_db_path,
+                              pretrained_model=None, GPU_ID=None):
+    solver, window_db = \
+        load_solver_and_window_db(solver_def_path,
+                                  window_db_path,
+                                  pretrained_model=pretrained_model)
+    caffe.set_phase_train()
+    caffe.set_mode_gpu()
+    if GPU_ID is not None:
+        caffe.set_device(GPU_ID)
+
+    # TODO(rbg): fix this temp hack
+    window_db = window_db[0:5008]
+
+    max_epochs = 100
+    dp = DeepPyramid(solver.net)
+    for epoch in xrange(max_epochs):
+        # TODO(rbg): shuffle window_db
+        for db_i in xrange(0, len(window_db), 4):
+            start_t = time.time()
+
+            minibatch_db = window_db[db_i:db_i + 4]
+            im_blob, rois_blob, labels_blob = \
+                finetuning.get_minibatch(minibatch_db)
+
+            # Reshape net's input blobs
+            base_shape = im_blob.shape
+            num_rois = rois_blob.shape[0]
+            solver.net.blobs['data'].reshape(base_shape[0], base_shape[1],
+                                             base_shape[2], base_shape[3])
+            solver.net.blobs['rois'].reshape(num_rois, 5, 1, 1)
+            solver.net.blobs['labels'].reshape(num_rois, 1, 1, 1)
+            # Copy data into net's input blobs
+            solver.net.blobs['data'].data[...] = im_blob
+            solver.net.blobs['rois'].data[...] = \
+                rois_blob[:, :, np.newaxis, np.newaxis]
+            solver.net.blobs['labels'].data[...] = \
+                labels_blob[:, np.newaxis, np.newaxis, np.newaxis]
+
+            # print 'epoch {:d} image {:d}'.format(epoch, db_i)
+            # print_label_stats(labels_blob)
+            solver.step(1)
+            # Periodically snapshot and test
+            # print 'Elapsed time: {:.4f}'.format(time.time() - start_t)
+
+
+if __name__ == '__main__':
+    CAFFE_MODEL = '/data/reference_caffe_nets/ilsvrc_2012_train_iter_310k'
+    SOLVER_DEF = './model-defs/pyramid_solver.prototxt'
+    WINDOW_DB = './data/window_file_voc_2007_trainval.txt.pz'
+    GPU_ID = 0
+    train_model_random_scales(SOLVER_DEF, WINDOW_DB,
+                              pretrained_model=CAFFE_MODEL,
+                              GPU_ID=GPU_ID)
