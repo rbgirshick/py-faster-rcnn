7d820c722a3ab13a820edcedcf911c9554b2d73a
Ross Girshick <ross.girshick@gmail.com>
2015-02-27 11:36:24 -0800

    bug fix to make area calcs consistent

--- utils/bbox.pyx
+++ utils/bbox.pyx
@@ -32,23 +32,23 @@ def bbox_overlaps(
     cdef unsigned int k, n
     for k in range(K):
         box_area = (
-            (query_boxes[k, 2] - query_boxes[k, 0]) *
-            (query_boxes[k, 3] - query_boxes[k, 1])
+            (query_boxes[k, 2] - query_boxes[k, 0] + 1) *
+            (query_boxes[k, 3] - query_boxes[k, 1] + 1)
         )
         for n in range(N):
             iw = (
                 min(boxes[n, 2], query_boxes[k, 2]) -
-                max(boxes[n, 0], query_boxes[k, 0])
+                max(boxes[n, 0], query_boxes[k, 0]) + 1
             )
             if iw > 0:
                 ih = (
                     min(boxes[n, 3], query_boxes[k, 3]) -
-                    max(boxes[n, 1], query_boxes[k, 1])
+                    max(boxes[n, 1], query_boxes[k, 1]) + 1
                 )
                 if ih > 0:
                     ua = float(
-                        (boxes[n, 2] - boxes[n, 0]) *
-                        (boxes[n, 3] - boxes[n, 1]) +
+                        (boxes[n, 2] - boxes[n, 0] + 1) *
+                        (boxes[n, 3] - boxes[n, 1] + 1) +
                         box_area - iw * ih
                     )
                     overlaps[n, k] = iw * ih / ua
