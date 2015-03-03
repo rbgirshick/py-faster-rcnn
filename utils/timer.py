0ed6585a6037bfc2c2f48c707474f0a560c845c6
Ross Girshick <ross.girshick@gmail.com>
2015-03-03 07:53:23 -0800

    simple timer class

---
+++ utils/timer.py
@@ -0,0 +1,22 @@
+import time
+
+class Timer(object):
+    def __init__(self):
+        self.total_time = 0.
+        self.calls = 0
+        self.start_time = 0.
+        self.diff = 0.
+        self.average_time = 0.
+
+    def tic(self):
+        self.start_time = time.clock()
+
+    def toc(self, average=True):
+        self.diff = time.clock() - self.start_time
+        self.total_time += self.diff
+        self.calls += 1
+        self.average_time = self.total_time / self.calls
+        if average:
+            return self.average_time
+        else:
+            return self.diff
