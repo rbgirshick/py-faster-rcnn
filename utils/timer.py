dbd1ebd6d7f2b80048fe124353a05337169e262e
Ross Girshick <ross.girshick@gmail.com>
2015-03-27 18:11:18 -0700

    BSD -> MIT license

--- utils/timer.py
+++ utils/timer.py
@@ -1,9 +1,8 @@
 # --------------------------------------------------------
 # Fast R-CNN
-# Copyright (c) Microsoft. All rights reserved.
-# Written by Ross Girshick, 2015.
-# Licensed under the BSD 2-clause "Simplified" license.
-# See LICENSE in the project root for license information.
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
 # --------------------------------------------------------
 
 import time
