c4199cb7935ca61aabf98562db7775e9b5988a2c
Ross Girshick <ross.girshick@gmail.com>
2015-04-01 09:10:45 -0700

    Fix attribution

--- src/utils/bbox.pyx
+++ src/utils/bbox.pyx
@@ -2,7 +2,7 @@
 # Fast R-CNN
 # Copyright (c) 2015 Microsoft
 # Licensed under The MIT License [see LICENSE for details]
-# Written by Ross Girshick
+# Written by Sergey Karayev
 # --------------------------------------------------------
 
 cimport cython
