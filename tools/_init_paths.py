4e3ccd49e0a6c67868af368a1afc25af06da4d91
Ross Girshick <ross.girshick@gmail.com>
2015-04-28 22:30:35 -0700

    minor refactoring

--- tools/_init_paths.py
+++ tools/_init_paths.py
@@ -5,18 +5,21 @@
 # Written by Ross Girshick
 # --------------------------------------------------------
 
+"""Set up paths for Fast R-CNN."""
+
 import os.path as osp
 import sys
+
+def add_path(path):
+    if path not in sys.path:
+        sys.path.insert(0, path)
 
 this_dir = osp.dirname(__file__)
 
 # Add caffe to PYTHONPATH
 caffe_path = osp.join(this_dir, '..', 'caffe-fast-rcnn', 'python')
+add_path(caffe_path)
 
-if caffe_path not in sys.path:
-  sys.path.insert(0, caffe_path)
-
+# Add lib to PYTHONPATH
 lib_path = osp.join(this_dir, '..', 'lib')
-
-if lib_path not in sys.path:
-  sys.path.insert(0, lib_path)
+add_path(lib_path)
