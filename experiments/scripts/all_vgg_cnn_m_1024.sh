ed4eb7d7bcc74c14fd08bd3479cfc34b50a227c9
Ross Girshick <ross.girshick@gmail.com>
2015-04-29 14:02:16 -0700

    run all experiments

---
+++ experiments/scripts/all_vgg_cnn_m_1024.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+GPU=$1
+NET=vgg_cnn_m_1024
+./experiments/scripts/default_${NET}.sh $GPU
+./experiments/scripts/multiscale_${NET}.sh $GPU
+./experiments/scripts/multitask_no_bbox_reg_${NET}.sh $GPU
+./experiments/scripts/no_bbox_reg_${NET}.sh $GPU
+./experiments/scripts/piecewise_${NET}.sh $GPU
+./experiments/scripts/svd_${NET}.sh $GPU
+./experiments/scripts/svm_${NET}.sh $GPU
